9f9009bdcc193c08958559947d912cd6fbbd9347
William Candillon <wcandillon@gmail.com>
2023-02-28 14:27:42 +0100

    :wrench:

--- example/src/Examples/Stickers/GestureHandler.tsx
+++ example/src/Examples/Stickers/GestureHandler.tsx
@@ -1,5 +1,5 @@
 import type { SkMatrix, SkRect } from "@shopify/react-native-skia";
-import { Skia, MatrixIndex } from "@shopify/react-native-skia";
+import { Skia } from "@shopify/react-native-skia";
 import React from "react";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import type { SharedValue } from "react-native-reanimated";
@@ -7,6 +7,8 @@ import Animated, {
   useAnimatedStyle,
   useSharedValue,
 } from "react-native-reanimated";
+
+import { rotateZ, toM4, translate, scale } from "./MatrixHelpers";
 
 interface GestureHandlerProps {
   matrix: SharedValue<SkMatrix>;
@@ -24,37 +26,28 @@ export const GestureHandler = ({
   const offset = useSharedValue(Skia.Matrix());
 
   const pan = Gesture.Pan().onChange((e) => {
-    const newMatrix = Skia.Matrix(matrix.value.get());
-    const m = Skia.Matrix();
-    m.translate(e.changeX, e.changeY);
-    newMatrix.concat(m);
-    matrix.value = newMatrix;
+    matrix.value = translate(matrix.value, e.changeX, e.changeY);
   });
 
-  // const rotate = Gesture.Rotation()
-  //   .onBegin((e) => {
-  //     origin.value = Skia.Point(e.anchorX, e.anchorY);
-  //     offset.value = matrix.value;
-  //   })
-  //   .onChange((e) => {
-  //     matrix.value = concat(offset.value, origin.value, [
-  //       { rotateZ: e.rotation },
-  //     ]);
-  //   });
+  const rotate = Gesture.Rotation()
+    .onBegin((e) => {
+      origin.value = Skia.Point(e.anchorX, e.anchorY);
+      offset.value = matrix.value;
+    })
+    .onChange((e) => {
+      matrix.value = rotateZ(offset.value, e.rotation, origin.value);
+    });
 
-  // const scale = Gesture.Pinch()
-  //   .onBegin((e) => {
-  //     origin.value = [e.focalX, e.focalY, 0];
-  //     offset.value = matrix.value;
-  //   })
-  //   .onChange((e) => {
-  //     matrix.value = concat(offset.value, origin.value, [{ scale: e.scale }]);
-  //   });
+  const pinch = Gesture.Pinch()
+    .onBegin((e) => {
+      origin.value = Skia.Point(e.focalX, e.focalY);
+      offset.value = matrix.value;
+    })
+    .onChange((e) => {
+      matrix.value = scale(offset.value, e.scale, origin.value);
+    });
 
   const style = useAnimatedStyle(() => {
-    const m = matrix.value.get();
-    const tx = m[MatrixIndex.TransX];
-    const ty = m[MatrixIndex.TransY];
     return {
       position: "absolute",
       left: x,
@@ -66,15 +59,14 @@ export const GestureHandler = ({
         { translateX: -width / 2 },
         { translateY: -height / 2 },
         {
-          matrix: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, tx, ty, 0, 1],
+          matrix: toM4(matrix.value),
         },
         { translateX: width / 2 },
         { translateY: height / 2 },
       ],
     };
   });
-  //const gesture = Gesture.Race(scale, rotate, pan);
-  const gesture = Gesture.Race(pan);
+  const gesture = Gesture.Race(pinch, rotate, pan);
   return (
     <GestureDetector gesture={gesture}>
       <Animated.View style={style} />
